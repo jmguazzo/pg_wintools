081f954b0dead86cfdcc0c5e0a3350d1273dec3a
jmguazzo <jmguazzo@gmail.com>
2015-11-15 22:05:01 -0500

    Improve functions signature & cleanup

--- pg_wintools/create_functions.sql
+++ pg_wintools/create_functions.sql
@@ -1,12 +1,12 @@
 
   CREATE OR REPLACE FUNCTION pg_write_event_log_entry(
-    text,
-    integer)
+    message text,
+    event_type integer)
   RETURNS void AS
 'pg_wintools', 'pg_write_event_log_entry'
   LANGUAGE c ;
 
-CREATE OR REPLACE FUNCTION pg_file_exists(text)
+CREATE OR REPLACE FUNCTION pg_file_exists(filename text)
   RETURNS boolean AS
 'pg_wintools', 'pg_file_exists'
   LANGUAGE c ;
@@ -18,16 +18,17 @@ CREATE OR REPLACE FUNCTION pg_hostname()
   LANGUAGE c ;
 
 CREATE OR REPLACE FUNCTION pg_read_registry(
-    text,
-    text,
-	text)
+   root text,
+   key_name text,
+   key_value text)
   RETURNS text AS
 'pg_wintools', 'pg_read_registry'
   LANGUAGE c ;
+  REVOKE ALL ON FUNCTION public.pg_read_registry(text,text,text) FROM public;
   
   CREATE OR REPLACE FUNCTION pg_fixed_drives_list()
   RETURNS SETOF RECORD AS
 'pg_wintools', 'pg_fixed_drives_list'
   LANGUAGE c ;
 
-  select * from pg_fixed_drives_list() lst (drive text,total_size int, free_space int)
+ 
